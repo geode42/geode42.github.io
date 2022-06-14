068d22d6c14b9275016dd81e86463137da9607e5
Geode42 <99060684+Geode42@users.noreply.github.com>
2022-06-14 15:27:07 -0400

    Add files via upload

---
+++ service_worker.js
@@ -0,0 +1,14 @@
+const cacheName = 'cache'
+const precacheResources = ['minesweeper.html', 'minesweeper.css', 'minesweeper.js', 'fancy_input.js', 'manifest.json', 'DMSans-Regular.woff2', 'DMSans-Bold.woff2', 'JetBrainsMono-Regular.woff2', 'JetBrainsMono-Bold.woff2', 'favicon.svg']
+
+self.addEventListener('install', (e) => {
+	e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)))
+})
+
+self.addEventListener('fetch', (e) => {
+	e.respondWith(
+		caches.match(e.request).then((cachedResponse) => {
+			if (cachedResponse) return cachedResponse
+		})
+	)
+})
